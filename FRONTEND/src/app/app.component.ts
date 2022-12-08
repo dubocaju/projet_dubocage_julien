92e2ce665e18965faeb2e95c4e07a75c280c6294
Julien Dubocage <dubocage.julien68@gmail.com>
2022-12-08 13:45:44 +0100

    init: ajout tp06

--- FRONTEND/src/app/app.component.ts
+++ FRONTEND/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tp05_dubocage_julien';
+  title = 'tp06_dubocage_julien';
 }
